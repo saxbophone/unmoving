6d76e63aa96d1c8b12a97a07bf8d64698f6661b4
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-10 18:23:15 +0100

    Got the skeleton of the fixed class I need to have a careful think about how the converting constructors will work Int can be used either for a plain integer or for a fixed-point number --an unfortunate dilemma!

--- sxpsxfp/include/sxpsxfp/Fixed.hpp
+++ sxpsxfp/include/sxpsxfp/Fixed.hpp
@@ -15,6 +15,25 @@
 #ifndef COM_SAXBOPHONE_SXPSXFP_SXPSXFP_HPP
 #define COM_SAXBOPHONE_SXPSXFP_SXPSXFP_HPP
 
-namespace com::saxbophone::sxpsxfp {}
+// NOTE: Blindly assuming that the PS1 environment declares itself as a freestanding implementation for this check!
+#if __STDC_HOSTED__
+#include <cstdint>
+#else
+#include <sys/types.h>
+#endif
+
+namespace com::saxbophone::sxpsxfp {
+    class Fixed {
+    public:
+        using UnderlyingType = int32_t;
+        /**
+         * @brief Default constructor, creates a Fixed instance with value 0.0
+         */
+        Fixed();
+
+    private:
+        UnderlyingType _raw_value;
+    };
+}
 
 #endif // include guard
