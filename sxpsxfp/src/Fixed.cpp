81bd6799b6f35682d82c3c2b4525ec2c6053aaed
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-10 19:54:51 +0100

    Remove method stubs for truly test-driven-development

--- sxpsxfp/src/Fixed.cpp
+++ sxpsxfp/src/Fixed.cpp
@@ -1,22 +1,4 @@
 #include <sxpsxfp/Fixed.hpp>
 
 
-namespace com::saxbophone::sxpsxfp {
-    constexpr Fixed::Fixed() {}
-
-    constexpr Fixed::Fixed(UnderlyingType) {}
-
-    constexpr Fixed::Fixed(double) {}
-
-    constexpr Fixed Fixed::from_integer(int) {
-        return {};
-    }
-
-    constexpr Fixed::operator UnderlyingType() const {
-        return {};
-    }
-
-    constexpr Fixed::operator float() const {
-        return {};
-    }
-}
+namespace com::saxbophone::sxpsxfp {}
