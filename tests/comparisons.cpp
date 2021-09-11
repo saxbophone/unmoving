cb50d86af128d7f36b524776db44e20994c4f497
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-11 18:00:54 +0100

    Pedantically initialise all test variables This is to placate Clang++ on Ubuntu

--- tests/comparisons.cpp
+++ tests/comparisons.cpp
@@ -5,121 +5,121 @@
 using namespace com::saxbophone::sxpsxfp;
 
 TEST_CASE("Fixed == Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool equal = foo == bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed != Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool unequal = foo != bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed == UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool equal = foo == bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed != UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool unequal = foo != bar;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType == Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool equal = bar == foo;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType != Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool unequal = bar != foo;
     SUCCEED();
 }
 
 TEST_CASE("Fixed < Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool less = foo < bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed > Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool greater = foo > bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed < UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool less = foo < bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed > UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool greater = foo > bar;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType < Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool less = bar < foo;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType > Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool greater = bar > foo;
     SUCCEED();
 }
 
 TEST_CASE("Fixed <= Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool less = foo <= bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed >= Fixed") {
-    Fixed foo, bar;
+    Fixed foo = {}, bar = {};
     bool greater = foo >= bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed <= UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool less = foo <= bar;
     SUCCEED();
 }
 
 TEST_CASE("Fixed >= UnderlyingType") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool greater = foo >= bar;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType <= Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool less = bar <= foo;
     SUCCEED();
 }
 
 TEST_CASE("UnderlyingType >= Fixed") {
-    Fixed foo;
-    Fixed::UnderlyingType bar;
+    Fixed foo = {};
+    Fixed::UnderlyingType bar = {};
     bool greater = bar >= foo;
     SUCCEED();
 }
