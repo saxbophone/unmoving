426d038f0a9a910758feea2dea5d87e9582b5adb
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-12 00:13:20 +0100

    Added stubs for user-defined literals for Fixed type

--- sxpsxfp/include/sxpsxfp/Fixed.hpp
+++ sxpsxfp/include/sxpsxfp/Fixed.hpp
@@ -57,6 +57,30 @@ namespace com::saxbophone::sxpsxfp {
             return {};
         }
         /**
+         * @brief User-defined literal for Fixed objects with fractional parts
+         *
+         * @b Usage:
+         * @code
+         * Fixed full = 123.45_fx;
+         * Fixed fractional = .45_fx;
+         * @endcode
+         */
+        constexpr friend Fixed operator"" _fx(long double literal);
+        /**
+         * @brief User-defined literal for Fixed objects without fractional parts
+         *
+         * @b Usage:
+         * @code
+         * Fixed integral = 123_fx;
+         * @endcode
+         * @warning Use this to initialise Fixed objects when the intention is
+         * to hold the same value as the integer. Use regular integer literals
+         * when the intention is to interpret the integer as a fixed-point value
+         * (this is the fixed-point equivalent of initialising a float from raw
+         * memory values).
+         */
+        constexpr friend Fixed operator"" _fx(unsigned long long int literal);
+        /**
          * @brief Implicit cast operator to underlying type
          */
         constexpr operator UnderlyingType() const {
