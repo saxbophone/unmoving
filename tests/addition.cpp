ca2bb0eb406144c4692be4085fb906fe58c773d5
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-12 15:06:43 +0100

    Wrote failing addition and subtraction tests

--- tests/addition.cpp
+++ tests/addition.cpp
@@ -3,17 +3,56 @@
 #include <sxpsxfp/Fixed.hpp>
 
 using namespace com::saxbophone::sxpsxfp;
+using Underlying = Fixed::UnderlyingType;
 
 TEST_CASE("Fixed += Fixed") {
-    Fixed foo = {};
-    Fixed bar = {};
+    // use half of range for both operands to avoid signed overflow UB
+    Underlying i = GENERATE(
+        take(
+            10,
+            random(
+                std::numeric_limits<Underlying>::min() / 2,
+                std::numeric_limits<Underlying>::max() / 2
+            )
+        )
+    );
+    Underlying j = GENERATE(
+        take(
+            10,
+            random(
+                std::numeric_limits<Underlying>::min() / 2,
+                std::numeric_limits<Underlying>::max() / 2
+            )
+        )
+    );
+    Fixed foo(i);
+    Fixed bar(j);
     foo += bar;
-    SUCCEED();
+    REQUIRE((Underlying)foo == i + j);
 }
 
 TEST_CASE("Fixed + Fixed") {
-    Fixed foo = {};
-    Fixed bar = {};
+    // use half of range for both operands to avoid signed overflow UB
+    Underlying i = GENERATE(
+        take(
+            10,
+            random(
+                std::numeric_limits<Underlying>::min() / 2,
+                std::numeric_limits<Underlying>::max() / 2
+            )
+        )
+    );
+    Underlying j = GENERATE(
+        take(
+            10,
+            random(
+                std::numeric_limits<Underlying>::min() / 2,
+                std::numeric_limits<Underlying>::max() / 2
+            )
+        )
+    );
+    Fixed foo(i);
+    Fixed bar(j);
     Fixed baz = foo + bar;
-    SUCCEED();
+    REQUIRE((Underlying)baz == i + j);
 }
