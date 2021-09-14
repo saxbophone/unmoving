b4b019f3bf68157a6fac6e922dfeb281ee9bcf65
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-14 23:11:55 +0100

    Rewrote multiplication tests for brevity

--- tests/multiplication.cpp
+++ tests/multiplication.cpp
@@ -9,64 +9,92 @@
 using namespace com::saxbophone::sxpsxfp;
 using Underlying = Fixed::UnderlyingType;
 
-TEST_CASE("Fixed *= Fixed") {
-    // max value is sqrt(max fixed point value) to prevent overflow
-    const double max_operand = std::sqrt(524287.9997558594);
-    double i = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    double j = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    Fixed foo(i);
-    Fixed bar(j);
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    auto expected_result = Approx((double)foo * (double)bar).margin(Fixed::FRACTIONAL_STEP);
-    CHECK((double)(foo *= bar) == expected_result);
-    REQUIRE((double)foo == expected_result);
-}
+TEST_CASE("Multiplication") {
+    double i = GENERATE(
+        take(
+            tests_config::ITERATIONS,
+            random(
+                -524288.9997558594,
+                524287.9997558594
+            )
+        )
+    );
 
-TEST_CASE("Fixed *= UnderlyingType") {
-    // max value is sqrt(max fixed point value) to prevent overflow
-    const double max_operand = std::sqrt(524287.9997558594);
-    double i = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    Underlying j = GENERATE_COPY(take(tests_config::ITERATIONS, random((Underlying)-max_operand, (Underlying)max_operand)));
-    Fixed foo(i);
-    Underlying bar = j;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    auto expected_result = Approx((double)foo * bar).margin(Fixed::FRACTIONAL_STEP);
-    CHECK((double)(foo *= bar) == expected_result);
-    REQUIRE((double)foo == expected_result);
-}
+    SECTION("Multiplication by Fixed") {
+        double j = GENERATE_COPY(
+            take(
+                1,
+                filter(
+                    // verify result does not exceed bounds of Fixed type
+                    [=](double u) {
+                        return -524288.9997558594 <= (i * u) and
+                            (i * u) <= 524287.9997558594;
+                    },
+                    random(
+                        -524288.9997558594,
+                        524287.9997558594
+                    )
+                )
+            )
+        );
 
-TEST_CASE("Fixed * Fixed") {
-    // max value is sqrt(max fixed point value) to prevent overflow
-    const double max_operand = std::sqrt(524287.9997558594);
-    double i = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    double j = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    Fixed foo(i);
-    Fixed bar(j);
-    Fixed baz = foo * bar;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)baz == Approx((double)foo * (double)bar).margin(Fixed::FRACTIONAL_STEP));
-}
+        Fixed foo(i);
+        Fixed bar(j);
+        auto expected_result = Approx((double)foo * (double)bar).margin(Fixed::FRACTIONAL_STEP);
 
-TEST_CASE("Fixed * UnderlyingType") {
-    // max value is sqrt(max fixed point value) to prevent overflow
-    const double max_operand = std::sqrt(524287.9997558594);
-    double i = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    Underlying j = GENERATE_COPY(take(tests_config::ITERATIONS, random((Underlying)-max_operand, (Underlying)max_operand)));
-    Fixed foo(i);
-    Underlying bar = j;
-    Fixed baz = foo * bar;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)baz == Approx((double)foo * bar).margin(Fixed::FRACTIONAL_STEP));
-}
+        SECTION("Fixed *= Fixed") {
+            CAPTURE(i, j, expected_result, (double)foo, (double)bar);
+            CHECK((double)(foo *= bar) == expected_result);
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)foo == expected_result);
+        }
 
-TEST_CASE("UnderlyingType * Fixed") {
-    // max value is sqrt(max fixed point value) to prevent overflow
-    const double max_operand = std::sqrt(524287.9997558594);
-    Underlying i = GENERATE_COPY(take(tests_config::ITERATIONS, random((Underlying)-max_operand, (Underlying)max_operand)));
-    double j = GENERATE_COPY(take(tests_config::ITERATIONS, random(-max_operand, max_operand)));
-    Underlying foo = i;
-    Fixed bar(j);
-    Fixed baz = foo * bar;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)baz == Approx(foo * (double)bar).margin(Fixed::FRACTIONAL_STEP));
+        SECTION("Fixed * Fixed") {
+            Fixed baz = foo * bar;
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)baz == expected_result);
+        }
+    }
+
+    SECTION("Multiplication by UnderlyingType") {
+        Underlying j = GENERATE_COPY(
+            take(
+                1,
+                filter(
+                    // verify result does not exceed bounds of Fixed type
+                    [=](Underlying u) {
+                        return -524288.9997558594 <= (i * u) and
+                            (i * u) <= 524287.9997558594;
+                    },
+                    random(
+                        -524288,
+                        524287
+                    )
+                )
+            )
+        );
+
+        Fixed foo(i);
+        Underlying bar = j;
+        auto expected_result = Approx((double)foo * bar).margin(Fixed::FRACTIONAL_STEP);
+
+        SECTION("Fixed *= UnderlyingType") {
+            CHECK((double)(foo *= bar) == expected_result);
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)foo == expected_result);
+        }
+
+        SECTION("Fixed * UnderlyingType") {
+            Fixed baz = foo * bar;
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)baz == expected_result);
+        }
+
+        SECTION("UnderlyingType * Fixed") {
+            Fixed baz = bar * foo;
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)baz == expected_result);
+        }
+
+    }
 }
