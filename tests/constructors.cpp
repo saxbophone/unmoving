f94348bea9d6c060df1236fd95725e3538cbcbe0
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-14 21:53:48 +0100

    Rewrote constructors tests for brevity

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -11,117 +11,104 @@
 using namespace com::saxbophone::sxpsxfp;
 using Underlying = Fixed::UnderlyingType;
 
-TEST_CASE("Default constructor can be called") {
-    Fixed foo = {};
-    REQUIRE((Underlying)foo == 0);
-}
+TEST_CASE("Constructors") {
+    SECTION("Default constructor can be called") {
+        Fixed foo = {};
+        REQUIRE((Underlying)foo == 0);
+    }
 
-TEST_CASE("Implicit conversion from fixed-point integer") {
-    Underlying i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                std::numeric_limits<Underlying>::min(),
-                std::numeric_limits<Underlying>::max()
+    SECTION("Conversion from fixed-point integer") {
+        Underlying i = GENERATE(
+            take(
+                tests_config::ITERATIONS,
+                random(
+                    std::numeric_limits<Underlying>::min(),
+                    std::numeric_limits<Underlying>::max()
+                )
             )
-        )
-    );
-    Fixed var = i;
-    REQUIRE((Underlying)var == i);
-}
+        );
 
-TEST_CASE("Explicit conversion from fixed-point integer") {
-    Underlying i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                std::numeric_limits<Underlying>::min(),
-                std::numeric_limits<Underlying>::max()
+        SECTION("Implicit conversion from fixed-point integer") {
+            Fixed var = i;
+            REQUIRE((Underlying)var == i);
+        }
+
+        SECTION("Explicit conversion from fixed-point integer") {
+            Fixed var(i);
+            REQUIRE((Underlying)var == i);
+        }
+
+        SECTION("Special advanced constructors") {
+            // Now for some more advanced stuff
+            SECTION("Assignment operator can be called") {
+                Fixed foo, bar(i);
+                foo = bar;
+                REQUIRE((Underlying)foo == (Underlying)bar);
+            }
+
+            SECTION("Copy constructor can be called") {
+                Fixed foo(i);
+                Fixed bar = foo;
+                REQUIRE((Underlying)foo == (Underlying)bar);
+            }
+
+            SECTION("Explicit copy constructor can be called") {
+                Fixed foo(i);
+                Fixed bar(foo);
+                REQUIRE((Underlying)foo == (Underlying)bar);
+            }
+        }
+    }
+
+    SECTION("Conversion from float") {
+        float i = GENERATE(
+            take(
+                tests_config::ITERATIONS,
+                random(
+                    -524288.9997558594f,
+                    524287.9997558594f
+                )
             )
-        )
-    );
-    Fixed var(i);
-    REQUIRE((Underlying)var == i);
-}
+        );
 
-TEST_CASE("Implicit conversion from float") {
-    float i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594f,
-                524287.9997558594f
+        SECTION("Implicit conversion from float") {
+            Fixed var = i;
+            REQUIRE((Underlying)var == (Underlying)round(i * 4096));
+        }
+
+        SECTION("Explicit conversion from float") {
+            Fixed var(i);
+            REQUIRE((Underlying)var == (Underlying)round(i * 4096));
+        }
+    }
+
+    SECTION("Conversion from double") {
+        double i = GENERATE(
+            take(
+                tests_config::ITERATIONS,
+                random(
+                    -524288.9997558594,
+                    524287.9997558594
+                )
             )
-        )
-    );
-    Fixed var = i;
-    REQUIRE((Underlying)var == (Underlying)round(i * 4096));
+        );
+
+        SECTION("Implicit conversion from double") {
+            Fixed var = i;
+            REQUIRE((Underlying)var == (Underlying)round(i * 4096));
+        }
+
+
+        SECTION("Explicit conversion from double") {
+            Fixed var(i);
+            REQUIRE((Underlying)var == (Underlying)round(i * 4096));
+        }
+    }
+
+    SECTION("Creation from integer value") {
+        Underlying i = GENERATE(take(tests_config::ITERATIONS, random(-524288, 524287)));
+        Fixed var = Fixed::from_integer(i);
+        REQUIRE((Underlying)var == i * 4096);
+    }
 }
 
-TEST_CASE("Implicit conversion from double") {
-    double i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
-            )
-        )
-    );
-    Fixed var = i;
-    REQUIRE((Underlying)var == (Underlying)round(i * 4096));
-}
-
-TEST_CASE("Explicit conversion from float") {
-    float i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594f,
-                524287.9997558594f
-            )
-        )
-    );
-    Fixed var(i);
-    REQUIRE((Underlying)var == (Underlying)round(i * 4096));
-}
-
-TEST_CASE("Explicit conversion from double") {
-    double i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
-            )
-        )
-    );
-    Fixed var(i);
-    REQUIRE((Underlying)var == (Underlying)round(i * 4096));
-}
-
-TEST_CASE("Creation from integer value") {
-    Underlying i = GENERATE(take(tests_config::ITERATIONS, random(-524288, 524287)));
-    Fixed var = Fixed::from_integer(i);
-    REQUIRE((Underlying)var == i * 4096);
-}
-
-// Now for some more advanced stuff
-
-TEST_CASE("Assignment operator can be called") {
-    Fixed foo, bar;
-    foo = bar;
-    REQUIRE((Underlying)foo == (Underlying)bar);
-}
-
-TEST_CASE("Copy constructor can be called") {
-    Fixed foo = {};
-    Fixed bar = foo;
-    REQUIRE((Underlying)foo == (Underlying)bar);
-}
-
-TEST_CASE("Explicit copy constructor can be called") {
-    Fixed foo = {};
-    Fixed bar(foo);
-    REQUIRE((Underlying)foo == (Underlying)bar);
-}
