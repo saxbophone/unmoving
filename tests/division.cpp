83cc4f2bb8227c59f0fe9efab9214cf8aaeb76e7
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-14 22:08:52 +0100

    Refactored division tests for brevity

--- tests/division.cpp
+++ tests/division.cpp
@@ -7,7 +7,7 @@
 using namespace com::saxbophone::sxpsxfp;
 using Underlying = Fixed::UnderlyingType;
 
-TEST_CASE("Fixed /= Fixed") {
+TEST_CASE("Division") {
     double i = GENERATE(
         take(
             tests_config::ITERATIONS,
@@ -17,83 +17,75 @@ TEST_CASE("Fixed /= Fixed") {
             )
         )
     );
-    double j = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
-            )
-        )
-    );
-    Fixed foo(i);
-    Fixed bar(j);
-    auto expected_result = Approx((double)foo / (double)bar).margin(Fixed::FRACTIONAL_STEP);
-    CAPTURE(i, j, expected_result, (double)foo, (double)bar);
-    CHECK((double)(foo /= bar) == expected_result);
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)foo == expected_result);
-}
 
-TEST_CASE("Fixed /= UnderlyingType") {
-    double i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
+    SECTION("Division by Fixed") {
+        double j = GENERATE_COPY(
+            take(
+                1,
+                filter(
+                    // verify result does not exceed bounds of Fixed type
+                    [=](double u) {
+                        return -524288.9997558594 <= (i / u) and
+                            (i / u) <= 524287.9997558594;
+                    },
+                    random(
+                        -524288.9997558594,
+                        524287.9997558594
+                    )
+                )
             )
-        )
-    );
-    Underlying j = GENERATE(take(tests_config::ITERATIONS, random(-524288, 524287)));
-    Fixed foo(i);
-    Underlying bar = j;
-    auto expected_result = Approx((double)foo / bar).margin(Fixed::FRACTIONAL_STEP);
-    CHECK((double)(foo /= bar) == expected_result);
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)foo == expected_result);
-}
+        );
 
-TEST_CASE("Fixed / Fixed") {
-    double i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
-            )
-        )
-    );
-    double j = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
-            )
-        )
-    );
-    Fixed foo(i);
-    Fixed bar(j);
-    Fixed baz = foo / bar;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)baz == Approx((double)foo / (double)bar).margin(Fixed::FRACTIONAL_STEP));
-}
+        Fixed foo(i);
+        Fixed bar(j);
+        auto expected_result = Approx((double)foo / (double)bar).margin(Fixed::FRACTIONAL_STEP);
 
-TEST_CASE("Fixed / UnderlyingType") {
-    double i = GENERATE(
-        take(
-            tests_config::ITERATIONS,
-            random(
-                -524288.9997558594,
-                524287.9997558594
+        SECTION("Fixed /= Fixed") {
+            CAPTURE(i, j, expected_result, (double)foo, (double)bar);
+            CHECK((double)(foo /= bar) == expected_result);
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)foo == expected_result);
+        }
+
+        SECTION("Fixed / Fixed") {
+            Fixed baz = foo / bar;
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)baz == expected_result);
+        }
+    }
+
+    SECTION("Division by UnderlyingType") {
+        Underlying j = GENERATE_COPY(
+            take(
+                1,
+                filter(
+                    // verify result does not exceed bounds of Fixed type
+                    [=](Underlying u) {
+                        return -524288.9997558594 <= (i / u) and
+                            (i / u) <= 524287.9997558594;
+                    },
+                    random(
+                        -524288,
+                        524287
+                    )
+                )
             )
-        )
-    );
-    Underlying j = GENERATE(take(tests_config::ITERATIONS, random(-524288, 524287)));
-    Fixed foo(i);
-    Underlying bar = j;
-    Fixed baz = foo / bar;
-    // allowed to deviate up to the smallest step in the fixed-point representation
-    REQUIRE((double)baz == Approx((double)foo / bar).margin(Fixed::FRACTIONAL_STEP));
+        );
+
+        Fixed foo(i);
+        Underlying bar = j;
+        auto expected_result = Approx((double)foo / bar).margin(Fixed::FRACTIONAL_STEP);
+
+        SECTION("Fixed /= UnderlyingType") {
+            CHECK((double)(foo /= bar) == expected_result);
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)foo == expected_result);
+        }
+
+        SECTION("Fixed / UnderlyingType") {
+            Fixed baz = foo / bar;
+            // allowed to deviate up to the smallest step in the fixed-point representation
+            REQUIRE((double)baz == expected_result);
+        }
+    }
 }
