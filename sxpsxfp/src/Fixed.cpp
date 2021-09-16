2da25bc10ab9f4a446aaab8782197bf60f6f25d9
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-16 05:34:06 +0100

    Mark source file for removal in Doxygen todo-list [no ci]

--- sxpsxfp/src/Fixed.cpp
+++ sxpsxfp/src/Fixed.cpp
@@ -1,4 +1,10 @@
 #include <sxpsxfp/Fixed.hpp>
 
+/**
+ * @file
+ * @todo When satisfied that this project's API is stabilised and there are no
+ * symbols we want in this source file, remove it and change the build to a
+ * strictly header-only build.
+ */
 
 namespace com::saxbophone::sxpsxfp {}
